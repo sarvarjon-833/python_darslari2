4c28819d2d6d06cfc04c95ad4f86e5a6e9af2e7d
NIYAZOV SARVARJON <sarvarjonniyazov833@gmail.com>
2025-03-17 15:30:44 +0900

    Create python darslari 8,9.py

---
+++ python darslari 8,9.py
@@ -0,0 +1,34 @@
+#ismlar=['Oybek','Bahora','Sherbek','Ilyos aka']
+#print(ismlar)
+#print("Salom " ,ismlar[1] , " bugun choyxonaga boramizmi?")
+#print('Salom', ismlar[2] , 'bugun futbol bormi')
+#print('Salom' , ismlar[3], ',' ' nima qilyapsiz?')
+
+#sonlar=[1,2,5,-4,67,23,13,10.1,]
+#print(sonlar[2] + 24)
+#print(sonlar)
+#sonlar[2]=25+ sonlar[2]
+#print(sonlar)
+#t_shaxslar=['Amir Temur', 'Boburmirzo','Islom Karimov']
+#z_shaxslar=['Teacher Azam', 'Jamshidxon Ziyoxonov',' Ustozation']
+#print('Men tarixiy shaxslardan ', t_shaxslar.pop(0), 'bilan', ', ','\nzamonaviy shaxslardan esa', z_shaxslar.pop(0), 'bilan suhbat qurishni istardim')
+#friends=[]
+#friends.append('Oybek')
+#print(friends)
+#friends.remove('Abdulxay')
+
+#mehmonlar= ['Ali', 'Vali',' Hadsan','Husan', 'Olim']
+#for mehmon in mehmonlar:
+    #print("salom", mehmon)
+    
+#mehmonlar= ['Ali', 'Vali',' Hadsan','Husan', 'Olim']
+#for mehmon in mehmonlar:
+    #print(f"Hurmatli {mehmon}, sizni 28 noyabr kuni nahorga oshga taklif qilamiz")
+   # print("Hurmat bilan, Palonchiyevlar oilasi")
+#sonlar=list(range(1,11))
+#for son in sonlar:
+    #print(f"{son}ning kvadrati {son**2}ga teng")
+
+
+
+
